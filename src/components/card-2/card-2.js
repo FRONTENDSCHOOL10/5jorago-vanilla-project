cefcd7b2e8a66949870b922604f1294f2ffc2d39
YSP97 <pus9717@gmail.com>
2024-07-10 13:15:21 +0900

    Feat: 메인페이지 마크업 섹션

--- src/components/card-2/card-2.js
+++ src/components/card-2/card-2.js
@@ -1,1 +1,25 @@
 import '/components/card-2/_card-2.scss';
+import { insertLast } from 'kind-tiger';
+
+const template = `
+<div class="card-section">
+      <div class="card">
+        <img class="card--img" src="../../assets/poster-1.png" alt="이미지" />
+        <div class="card--wrapper">
+          <span class="card__wrapper--rank">1</span>
+          <div class="scrap">
+            <span class="card__wrapper--title">재벌집 막내아들</span>
+            <div class="dot"><span class="card__wrapper--dot is-active"></span></div>
+          </div>
+        </div>
+      </div>
+    </div>`;
+
+insertLast('.sec-3', template);
+insertLast('.sec-3-1', template);
+insertLast('.sec-3-2', template);
+insertLast('.sec-3-3', template);
+insertLast('.sec-3-4', template);
+insertLast('.sec-3-5', template);
+insertLast('.sec-3-6', template);
+insertLast('.sec-3-7', template);
