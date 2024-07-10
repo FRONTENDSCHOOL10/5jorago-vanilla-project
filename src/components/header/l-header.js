dca2c9288db0f86c0712d432ac6b253e057fb609
kwonboryong <kwonboryong@naver.com>
2024-07-10 17:50:52 +0900

    Feat: 웹 컴포넌트 랜딩 헤더 추가, Remove: 웹 컴포넌트 폴더 삭제

---
+++ src/components/header/l-header.js
@@ -0,0 +1,24 @@
+import css from '/src/components/header/_header.scss?inline';
+import logo from '/src/assets/logo_1.png';
+
+// 랜딩 페이지 컴포넌트
+export class Header extends HTMLElement {
+  constructor() {
+    super();
+    this.attachShadow({ mode: 'open' });
+    this.shadowRoot.innerHTML = `
+      <style>${css}</style>
+      <header class="header">
+      <nav class="nav">
+        <div class="header__nav__logo-menu">
+          <a href="#" class="header__logo">
+            <img src="${logo}" alt="Logo" class="header__logo" />
+          </a>
+        </div>
+      </nav>
+    </header>
+    `;
+  }
+}
+
+customElements.define('c-header', Header);
