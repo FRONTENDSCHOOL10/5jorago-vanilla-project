68b4a13724e77885d216acb71f76e7bc43443b8a
hyoungsiyoung <hyoungsiyoung@gmail.com>
2024-07-10 17:36:24 +0900

    Refactor: header 컴포넌트 리팩토링

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -7,39 +7,39 @@ export class Header extends HTMLElement {
     this.attachShadow({ mode: 'open' });
     this.shadowRoot.innerHTML = `
       <style>${css}</style>
-      <header class="header">
+          <header class="header">
       <nav class="nav">
         <div class="header__nav__logo-menu">
           <a href="#" class="header__logo">
-            <img src="${logo}" alt="Logo" class="header__logo" />
+            <img src="/assets/logo_1.png" alt="Logo" class="header__logo" />
           </a>
 
           <div class="header__menu-wrapper">
             <ul class="header__menu">
-              <li style="list-style: none">
+              <li>
                 <a href="#" class="header__menu-item" aria-label="Live">
                   <img
-                    src="/src/assets/live_1_default.png"
+                    src="/assets/live_1_default.png"
                     alt="Live Icon"
                     class="header__icon-live"
                   />
                   <span class="text">실시간</span>
                 </a>
               </li>
-              <li style="list-style: none">
+              <li>
                 <a href="#" class="header__menu-item" aria-label="TV Programs">
                   <span class="text">TV 프로그램</span>
                 </a>
               </li>
-              <li style="list-style: none">
+              <li>
                 <a href="#" class="header__menu-item" aria-label="Movies">
                   <span class="text">영화</span>
                 </a>
               </li>
-              <li style="list-style: none">
+              <li>
                 <a href="#" class="header__menu-item" aria-label="Paramount">
                   <img
-                    src="/src/assets/paramount_1_default.png"
+                    src="/assets/paramount_1_default.png"
                     alt="Paramount Icon"
                     class="header__paramount"
                   />
@@ -52,14 +52,14 @@ export class Header extends HTMLElement {
         <div class="aside">
           <a href="#" aria-label="Search">
             <img
-              src="/src/assets/search_1_default.png"
+              src="/assets/search_1_default.png"
               alt="Search Icon"
               class="header__icon-search"
             />
           </a>
           <a href="#" aria-label="Profile">
             <img
-              src="/src/assets/usericon_1.png"
+              src="/assets/usericon_1.png"
               alt="Profile Icon"
               class="header__icon-profile"
             />
