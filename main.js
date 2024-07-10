2bdf17118ab0f1172947d4f95c71305de53bcfaf
kwonboryong <kwonboryong@naver.com>
2024-07-11 00:17:42 +0900

    Style: 랜딩 페이지 마크업 및 스타일 수정, 랜딩 버튼 컴포넌트 생성

--- main.js
+++ main.js
@@ -6,16 +6,18 @@ import pb from '/src/api/pocketbase.js';
 import { Header } from '/src/components/header/l-header.js'
 import { Footer } from '/src/components/footer/footer.js'
 
+// 랜딩 버튼 컴포넌트
+import '/src/components/rendingbutton/rendingbutton.js'
+
 
 // 스와이퍼
-// import Swiper from 'swiper';
-// import 'swiper/css';
-
 import Swiper from 'swiper/bundle';
 import 'swiper/css/bundle';
 
 // 스와이퍼
 document.addEventListener('DOMContentLoaded', function () {
+
+  // 섹션2 슬라이드
   const swiper = new Swiper('.swiper', {
     direction: 'horizontal',
     loop: true,
@@ -28,6 +30,24 @@ document.addEventListener('DOMContentLoaded', function () {
     speed: 6000, // 슬라이드 전환 속도
     slidesPerView: 1, // 화면에 표시할 슬라이드 수
     spaceBetween: -485, // 이미지 간격
+    centeredSlides: true, // 슬라이드를 가운데 정렬
+  });
+
+
+  // 섹션3 슬라이드
+  const swiper2 = new Swiper('.swiper2', {
+    direction: 'horizontal',
+    loop: true,
+
+    autoplay: {
+      delay: 0,
+      disableOnInteraction: false,
+    },
+
+    speed: 4000, // 슬라이드 전환 속도
+    slidesPerView: 1, // 화면에 표시할 슬라이드 수
+    spaceBetween: -1650, // 이미지 간격
+    // spaceBetween: -1115, // 이미지 간격
     centeredSlides: true, // 슬라이드를 가운데 정렬
   });
 });
--- src/components/rendingbutton/rendingbutton.js
+++ src/components/rendingbutton/rendingbutton.js
@@ -1,2 +1,15 @@
 import '/src/components/rendingbutton/_rendingbutton.scss';
+import logo from '/src/assets/icon.png';
 
+const template = `
+<div class="rending">
+<button class="rending__button">
+  <img src="${logo}" alt="" srcset="" />
+  <span>새로워진 티빙을 만나보세요!</span>
+</button>
+</div>
+`;
+
+
+document.querySelector('.b1 .rendingbutton').innerHTML = template;
+document.querySelector('.b4 .rendingbutton').innerHTML = template;
