5b38154e98269a77f9c3f0ab5fb81e03be940585
YSP97 <pus9717@gmail.com>
2024-07-14 23:58:09 +0900

    Feat: 서치 페이지 포켓베이스 랭킹 데이터 바인딩 - 서치페이지 랭킹 순서대로 데이터 바인딩 - 서치페이지 index.html를 template로 제작 - 서치페이지 최근 검색어 li 스타일 추가

---
+++ src/pages/search/ranking.js
@@ -0,0 +1,39 @@
+import pb from '/src/api/pocketbase.js';
+import { getNode, insertLast } from 'kind-tiger';
+
+async function renderDate() {
+  const data = await pb.collection('main_ranking').getFullList({
+    sort: 'rank',
+  });
+  const rankingContainer = getNode('.search__popular-searches--list');
+
+  for (let i = 0; i < data.length; i++) {
+    const dataObj = data[i];
+    console.log(dataObj);
+
+    const template = `
+  <span class="search__popular-searches--rank">${dataObj.rank}</span>
+  <a href="/src/pages/search/" class="search__popular-searches--content-title">
+    <h3>${dataObj.title}</h3>
+  </a>`;
+
+    const elem = document.createElement('li');
+    elem.className = `search__popular-searches--content`;
+    rankingContainer.appendChild(elem);
+
+    insertLast(elem, template);
+  }
+  let today = new Date();
+  let year = today.getFullYear();
+  let month = today.getMonth() + 1;
+  let date = today.getDate();
+  let hours = today.getHours();
+  let minutes = today.getMinutes();
+
+  const dateElement = document.createElement('span');
+  dateElement.className = 'search__date';
+  dateElement.textContent = `${year + '.' + month + '.' + date + ' 오전 ' + hours + ':' + minutes + ' 기준'}`;
+  rankingContainer.appendChild(dateElement);
+}
+
+renderDate();
