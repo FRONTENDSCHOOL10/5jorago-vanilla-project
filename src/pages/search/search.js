dfc75737a3f0234092281d8c425f0af924647899
YSP97 <pus9717@gmail.com>
2024-07-15 00:20:09 +0900

    Feat: 최근 검색어 구현 - 최근 검색어 추가되도록 구현 - x 버튼 누를 시 검색기록 삭제

--- src/pages/search/search.js
+++ src/pages/search/search.js
@@ -1,49 +1,9 @@
 import '/src/pages/search/_search.scss';
 import { Header } from '/src/components/header/header.js';
-import { getNode } from 'kind-tiger';
+import { getNode, insertLast } from 'kind-tiger';
 import pb from '/src/api/pocketbase.js';
 import getPbImageURL from '/api/getPbImageURL';
+import { handleRecentSearch } from './recentSearch';
+import {} from './ranking';
 
-
-// debounce 함수
-function debounce(func, wait) {
-  let timeout;
-  return function (...args) {
-    const later = () => {
-      clearTimeout(timeout);
-      func.apply(this, args);
-    };
-    clearTimeout(timeout);
-    timeout = setTimeout(later, wait);
-  };
-}
-
-
-// 인풋값 
-
-async function handleInput(event) {
-  const collections = [
-    'main_must_watch',
-    'main_only_taing',
-    'main_ranking',
-    'main_vod',
-  ];
-
-  for (const collection of collections) {
-    const data = await pb.collection(collection).getFullList();
-
-    for (let i = 0; i < data.length; i++) {
-      const dataObj = data[i];
-      if (data.title === event.target.value) {
-        console.log(getPbImageURL(dataObj));
-      }
-    }
-  }
-}
-
-document.addEventListener('DOMContentLoaded', () => {
-  const searchInput = getNode('.search-input');
-  const debouncedInput = debounce(handleInput, 300);
-
-  searchInput.addEventListener('input', debouncedInput);
-});
+// searchButton.addEventListener('click', handleRecentSearch);
