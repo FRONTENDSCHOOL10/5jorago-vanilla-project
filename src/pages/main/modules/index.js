5ca614165ebaff68e309e40680b495f20c8e7c15
YSP97 <pus9717@gmail.com>
2024-07-15 11:16:30 +0900

    Docs: 폴더구조 및 파일 삭제 - 컴포넌트 index 파일 삭제 - main 페이지 js 파일 관리를 위한 index 파일 생성, modules 폴더 생성

---
+++ src/pages/main/modules/index.js
@@ -0,0 +1,9 @@
+import { Header } from '/src/components/header/header.js';
+import { Footer } from '/src/components/footer/footer.js';
+import '/src/components/card-2/card-2.js';
+import '/src/components/card-1/card-1.js';
+import '/src/components/card-3/card-3.js';
+import '/src/components/banner/banner.js';
+import '/src/components/card-4/card-4.js';
+import '/src/pages/main/modules/modal.js';
+import '/src/pages/main/modules/swiper.js';
