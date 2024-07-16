31b33abf149ee7df4f0544efaf987f5291cdde7b
hyoungsiyoung <hyoungsiyoung@gmail.com>
2024-07-16 21:29:13 +0900

    Feat: 로그인, 아이디 찾기, 비밀번호 찾기 js

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -1,76 +1,3 @@
-// import '/src/pages/login/_login.scss';
-
-// import { getStorage, setStorage } from 'kind-tiger';
-// import { Header } from '/src/components/header/l-header.js';
-// import { Footer } from '/src/components/footer/footer.js';
-
-// import pb from '/src/api/pocketbase.js';
-
-// document.addEventListener('DOMContentLoaded', () => {
-//   const userIdInput = document.querySelector('#user-id');
-//   const passwordInput = document.querySelector('#user-pw');
-//   const findIdButton = document.querySelector('#findID');
-//   const findPWButton = document.querySelector('#findPW');
-//   const loginButton = document.querySelector('.login-btn');
-//   const singupButton = document.querySelector('#signup-btn');
-
-//   loginButton.addEventListener('click', async (event) => {
-//     event.preventDefault();
-
-//     const userId = userIdInput.value;
-//     const password = passwordInput.value;
-
-//     if (userId.length == 0) {
-//       alert('아이디를 입력해주세요');
-//       userIdInput.focus();
-//       return;
-//     }
-
-//     if (password.length == 0) {
-//       alert('비밀번호를 입력해주세요');
-//       passwordInput.focus();
-//       return;
-//     }
-
-//     pb.collection('users')
-//       .authWithPassword(userId, password)
-//       .then(
-//         async () => {
-//           const { model, token } = await getStorage('pocketbase_auth');
-
-//           setStorage('auth', {
-//             isAuth: !!model,
-//             user: model,
-//             token,
-//           });
-
-//           const userID = JSON.parse(localStorage.getItem('auth')).user.username;
-
-//           alert('환영합니다. ' + userID + '님');
-//           location.href = '/src/pages/main/index.html';
-//         },
-//         () => {
-//           userIdInput.value = ''; // 입력값 초기화
-//           passwordInput.value = ''; // 입력값 초기화
-
-//           alert('아이디 또는 비밀번호가 일치하지 않습니다.');
-//         }
-//       );
-//   });
-
-//   findIdButton.addEventListener('click', () => {
-//     location.href = '/src/pages/findID/index.html';
-//   });
-
-//   findPWButton.addEventListener('click', () => {
-//     location.href = '/src/pages/findPW/index.html';
-//   });
-
-//   singupButton.addEventListener('click', () => {
-//     location.href = '/src/pages/signup/index.html';
-//   });
-// });
-
 import '/src/pages/login/_login.scss';
 
 import { getStorage, setStorage } from 'kind-tiger';
@@ -84,6 +11,9 @@ document.addEventListener('DOMContentLoaded', () => {
   const findIdButton = document.querySelector('#findID');
   const findPWButton = document.querySelector('#findPW');
   const loginButton = document.querySelector('.login-btn');
+  const passwordToggle = document.querySelector('.visible-icon');
+  const cancelIdToggle = document.querySelector('.cancel-id-icon');
+  const cancelPwToggle = document.querySelector('.cancel-pw-icon');
 
   const userIdError = document.querySelector('#user-id-error');
   const passwordError = document.querySelector('#user-pw-error');
@@ -106,8 +36,24 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   }, 1000);
 
-  userIdInput.addEventListener('input', validateInput);
-  passwordInput.addEventListener('input', validateInput);
+  const updateVisibility = () => {
+    cancelIdToggle.style.display =
+      userIdInput.value.length === 0 ? 'none' : 'block';
+    cancelPwToggle.style.display =
+      passwordInput.value.length === 0 ? 'none' : 'block';
+    passwordToggle.style.display =
+      passwordInput.value.length === 0 ? 'none' : 'block';
+  };
+
+  userIdInput.addEventListener('input', () => {
+    validateInput();
+    updateVisibility();
+  });
+
+  passwordInput.addEventListener('input', () => {
+    validateInput();
+    updateVisibility();
+  });
 
   loginButton.addEventListener('click', async (event) => {
     event.preventDefault();
@@ -142,7 +88,7 @@ document.addEventListener('DOMContentLoaded', () => {
           const userName = JSON.parse(localStorage.getItem('auth')).user.name;
 
           alert('환영합니다. ' + userName + '님');
-          location.href = '/src/pages/main/index.html';
+          location.href = '/src/pages/profile/index.html';
         },
         () => {
           userIdInput.value = ''; // 입력값 초기화
@@ -159,6 +105,29 @@ document.addEventListener('DOMContentLoaded', () => {
 
   findPWButton.addEventListener('click', () => {
     location.href = '/src/pages/findPW/index.html';
+  });
+
+  // 비밀번호 토글 기능
+  passwordToggle.addEventListener('click', () => {
+    const type =
+      passwordInput.getAttribute('type') === 'password' ? 'text' : 'password';
+    passwordInput.setAttribute('type', type);
+    passwordToggle.src =
+      type === 'password'
+        ? '/public/assets/unvisibleId_1.png'
+        : '/public/assets/visibleId_1.png';
+  });
+
+  cancelIdToggle.addEventListener('click', () => {
+    userIdInput.value = ''; // 입력값 초기화
+    validateInput();
+    updateVisibility();
+  });
+
+  cancelPwToggle.addEventListener('click', () => {
+    passwordInput.value = ''; // 입력값 초기화
+    validateInput();
+    updateVisibility();
   });
 });
 
