57d43f45d1b9baef2c37070df946090acf273ff8
Gomyeunghan <rhaudgks04@gmail.com>
2024-07-17 00:35:41 +0900

    Fix: components card-4,search 반응형 수정

--- src/pages/search/search.js
+++ src/pages/search/search.js
@@ -6,3 +6,15 @@ import getPbImageURL from '/src/api/getPbImageURL';
 import './modules/recentSearch';
 import './modules/ranking';
 import './modules/renderInput';
+
+const searchInput = getNode('.search-input');
+
+window.addEventListener('resize', function () {
+  const windowWidth = window.innerWidth;
+  if (windowWidth < 768) {
+    searchInput.placeholder = '검색';
+  } else {
+    searchInput.placeholder =
+      'TV프로그램, 영화 제목 및 출연진으로 검색해보세요.'; // 원래 placeholder로 되돌림
+  }
+});
