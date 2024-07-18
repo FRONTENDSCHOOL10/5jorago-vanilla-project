49449298f87c93e98e6b3516f891896f7ce15107
YSP97 <pus9717@gmail.com>
2024-07-18 13:38:25 +0900

    Feat: 로딩창구현

--- src/pages/main/modules/index.js
+++ src/pages/main/modules/index.js
@@ -5,6 +5,7 @@ import '/src/components/card-1/card-1.js';
 import '/src/components/card-3/card-3.js';
 import '/src/components/banner/banner.js';
 import '/src/components/card-4/card-4.js';
+import '/src/components/loading/loading.js';
 // import '/src/components/profileModal/profileModal.js';
 import '/src/pages/main/modules/modal.js';
 import '/src/pages/main/modules/swiper.js';
